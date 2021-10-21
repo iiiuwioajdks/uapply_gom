b0d3c4bbe813799ab17941df5a5e428d88b85784
laimuxi <1721261216@qq.com>
2021-10-21 23:12:07 +0800

    Add: 跨域

--- web/initialize/router.go
+++ web/initialize/router.go
@@ -2,13 +2,14 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"uapply_go/web/middleware"
 	"uapply_go/web/router"
 )
 
 // InitRouter 路由初始化
 func InitRouter() *gin.Engine {
 	Router := gin.New()
-	Router.Use(GinLogger(), GinRecovery(true))
+	Router.Use(GinLogger(), GinRecovery(true), middleware.Cors())
 
 	ApiRouter := Router.Group("/uapply/v1")
 	router.InitUserRouter(ApiRouter)
