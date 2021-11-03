e1799b2fb9d13573818a9f711564bd66bc34130f
laimuxi <1721261216@qq.com>
2021-11-03 21:23:43 +0800

    Fix: 管理员表结构修改

--- web/models/adminInfo.go
+++ web/models/adminInfo.go
@@ -1,18 +1,39 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
+type DepartmentBase struct {
+	DepartmentID uint `gorm:"primarykey"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
+}
+
+// Department 部门表
 type Department struct {
-	gorm.Model
-	OrganizationID uint   `gorm:"index:idx_orgid;unique;type:int;not null"`
+	// 直接用id作为departmentId，这样可以索引最优化
+	DepartmentBase
+	OrganizationID uint   `gorm:"index;type:int;not null"`
 	DepartmentName string `gorm:"type:varchar(100);not null"`
 	Account        string `gorm:"unique;type:varchar(20);not null"`
 	Password       string `gorm:"type:varchar(20);not null"`
 	Role           int8   `gorm:"type:int;not null comment '0代表管理员，1代表超级管理员'"`
 }
 
+type OrganizationBase struct {
+	OrganizationID uint `gorm:"primarykey"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
+}
+
+// Organization 组织表
 type Organization struct {
-	gorm.Model
+	// 直接用id作为organizationId，这样可以索引最优化
+	OrganizationBase
 	OrganizationName string `gorm:"type:varchar(100);not null"`
 	Department       []Department
 }
