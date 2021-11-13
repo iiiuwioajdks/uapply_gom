f99639487c0eaa8c462998d5eda8db054280a261
laimuxi <1721261216@qq.com>
2021-11-13 13:33:13 +0800

    ADD: 增加一些细节

--- web/validator/validator.go
+++ web/validator/validator.go
@@ -5,21 +5,26 @@ import (
 	"regexp"
 )
 
+const (
+	EMAIL = `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
+	PHONE = `^1([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`
+)
+
 // ValidateMobile 自定义手机验证
 func ValidateMobile(fl validator.FieldLevel) bool {
 	mobile := fl.Field().String()
 	// 手机号码正则表达式验证
-	ok, _ := regexp.MatchString(`^1([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`, mobile)
-	if ok {
-		return true
-	}
-	return false
+	return ValidateFunc(mobile, PHONE)
 }
 
 func ValidateEmail(fl validator.FieldLevel) bool {
 	email := fl.Field().String()
 	// 邮箱正则验证
-	ok, _ := regexp.MatchString(`^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`, email)
+	return ValidateFunc(email, EMAIL)
+}
+
+func ValidateFunc(val string, rule string) bool {
+	ok, _ := regexp.MatchString(rule, val)
 	if ok {
 		return true
 	}
