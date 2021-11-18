902ba55290048e0d78b47074e310a0d554da128a
laimuxi <1721261216@qq.com>
2021-11-18 22:07:09 +0800

    ADD:增加注释

--- web/forms/wx_login.go
+++ web/forms/wx_login.go
@@ -3,5 +3,5 @@ package forms
 type WxSession struct {
 	SessionKey string `json:"session_key"`
 	ExpireIn   int64  `json:"expires_in"`
-	UnionID    string `json:"unionid"`
+	OpenID     string `json:"openid"`
 }
